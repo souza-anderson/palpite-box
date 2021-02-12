634353ac94921f5b47b1ad97bb326cb6f65bb3a5
souza-anderson <andersonfebs@gmail.com>
2021-02-12 16:43:21 -0400

    adding description 'sobre'

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import React from 'react'
 import PageTitle from '../components/PageTitle'
 
@@ -6,12 +5,9 @@ const Sobre = () => {
   return (
     <div>
       <PageTitle title={'Sobre'} />
-      <h1>Sobre</h1>
-      <div>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </div>
+      <h1 className="text-center font-bold my-4 text-4x1">O que é este projeto?</h1>
+      <p className='text-center mb-4'>Este projeto tem como objetivo oferecer um serviço de sugestões para melhorias do estabelecimento</p>
+
     </div>
   )
 }
